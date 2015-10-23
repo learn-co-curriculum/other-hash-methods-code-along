4746bc0b13ccee4fb8235180afbe5267b07a7153
joshuabamboo <joshuaowens011@gmail.com>
2015-10-23 18:21:03 -0400

    create directory structure

---
+++ lib/min.rb
@@ -0,0 +1,10 @@
+def get_the_min
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+   vegetable: ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]
+  }
+
+  #code your solution here!
+end
