2edcd1c26a16cd2688e7d5f12569597c49cfa84c
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-30 10:22:01 -0500

    commit tree d006817ae7cc7a6f0c921392ac84cfd2376ddceb

--- lib/keys.rb
+++ lib/keys.rb
@@ -10,4 +10,5 @@ require 'pry'
 
 def get_the_keys(groceries)
   #code your solution here!
+  groceries.keys
 end
--- lib/min.rb
+++ lib/min.rb
@@ -10,4 +10,5 @@ require 'pry'
 
 def get_the_min(groceries)
   #code your solution here!
+  groceries.values.flatten.min
 end
